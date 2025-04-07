bab48cf856b98d8e4d2360d60a4fa029263e8b99
MaseeSakhi <maseesakhi2007@hotmail.com>
2025-04-07 14:22:49 +0200

    implented cardfinding-algorithm

--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] cards = [3,5,7,8,9];
+int querry =7;
+
+var result = FindcardLocation(cards,querry);
+Console.WriteLine(result);
+int FindcardLocation(int[]cards,int querry)
+{
+   /* 
+    Definiere Variable "index" und initialisiere mit Wert 0
+    Wiederhole bis "index" gleich "cards" length:
+        Wenn "cards" an "index" gleich "query", dann:
+            Gebe "index" zurück
+    Inkrementiere "index" um 1
+    */
+    int index=0;
+    while (index<cards.Length)
+    {
+      if (cards[index] == querry)
+            return index;
+        index++;  //index =index +1
+    }
+    return -1;
+
+}
